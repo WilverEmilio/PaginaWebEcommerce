878a56da08eb276abad666cd6641a582ce2303fd
Wilver Emilio Xiá Ixcot <wilb123@umes.edu.gt>
2026-01-03 21:55:54 -0600

    fix: configuración de backend URL para producción

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-    images: {
-    unoptimized: true,
+  images: {
+    domains: ['ecommercehealthy.up.railway.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ecommercehealthy.up.railway.app',
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
   },
 }
 
 module.exports = nextConfig
-  
